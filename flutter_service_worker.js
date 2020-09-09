2e94d1ee7a550448b5769f7849fead41cade033a
Teekam Singh <teekams10999@gmail.com>
2020-09-09 17:30:35 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const RESOURCES = {
 "/assets/assets/mypic.jpg": "0181869294a2470389553a7719845903",
 "/assets/FontManifest.json": "48092a100b610a70b734a6568c2ceee8",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "99cd663f2a928c41d290fae781e2cfa7",
+"/assets/LICENSE": "efd4f822a4e4d31fdaee33ac15816727",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/packages/flutter_signin_button/assets/logos/2.0x/google_dark.png": "937022ea241c167c8ce463d2ef7ed105",
 "/assets/packages/flutter_signin_button/assets/logos/2.0x/google_light.png": "8f10eb93525f0c0259c5e97271796b3c",
@@ -20,7 +20,7 @@ const RESOURCES = {
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "daf2084a942668e2f412f666cc83df51",
-"/main.dart.js": "efd4fdc0dbb6b57144181f25df4973f2",
+"/main.dart.js": "5b8dbe66ccc569958c2acd99d1ba11d3",
 "/manifest.json": "43b77533487762ba5654a181ea2dec2f"
 };
 
